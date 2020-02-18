b6e1a912514b31f9e493633215e4d903e1d32283
Bradley Broom <bmbroom@mdanderson.org>
2020-02-18 16:43:31 -0600

    Before switch to vanodi.js

---
+++ plot3d.js
@@ -0,0 +1,250 @@
+"use strict";
+
+var scene = new THREE.Scene();
+var controls = null;
+var camera = null;
+
+
+var renderer = new THREE.WebGLRenderer();
+
+var geometry = new THREE.SphereGeometry();
+var bigGeometry = new THREE.SphereGeometry(2);
+const highlightMaterial = new THREE.MeshBasicMaterial( { color: '#ffffff', transparent: true, opacity: 0.5 } );
+
+var raycaster = new THREE.Raycaster(), INTERSECTED;
+var mouse = new THREE.Vector2();
+
+const axesMaterial = new THREE.LineBasicMaterial( { color: 0x808080, transparent: true, opacity: 0.5 } );
+const axesGeometry = new THREE.Geometry();
+	axesGeometry.vertices.push (new THREE.Vector3( -100, -100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100,  100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100,  100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100, -100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100, -100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100, -100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100,  100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100,  100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100, -100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100, -100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100,  100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3( -100,  100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100,  100, -100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100,  100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100, -100,  100) );
+	axesGeometry.vertices.push (new THREE.Vector3(  100, -100, -100) );
+const axesObject = new THREE.Line (axesGeometry, axesMaterial);
+scene.add (axesObject);
+
+var makeLabel = null;
+const fontLoader = new THREE.FontLoader();
+fontLoader.load ('fonts/helvetiker_regular.typeface.json', function ( font ) {
+	makeLabel = function ( labelText ) {
+		return new THREE.TextGeometry( labelText, {
+			font: font,
+			size: 4,
+			height: 1,
+			curveSegments: 12,
+			bevelEnabled: false,
+			bevelThickness: 10,
+			bevelSize: 8,
+			bevelOffset: 0,
+			bevelSegments: 5
+		} );
+	};
+});
+const axisLabelMaterial = new THREE.MeshBasicMaterial( { color: '#808080', transparent: false } );
+const axisLabels = [];
+function addLabel (axis, labelText) {
+	if (makeLabel) {
+		const labelDist = 105;
+		const labelMesh = makeLabel ("-  " + labelText + "  +");
+		labelMesh.computeBoundingBox();
+		const labelCenter = (labelMesh.boundingBox.max.x - labelMesh.boundingBox.min.x) / 2.0;
+		const positiveMesh = makeLabel ("+");
+		const negativeMesh = makeLabel ("-");
+		const [ ax1, ax2 ] = [ 'x', 'y', 'z' ].filter (x => x !== axis);
+		addAxisLabel (labelDist, labelDist);
+		addAxisLabel (labelDist, -labelDist);
+		addAxisLabel (-labelDist, -labelDist);
+		addAxisLabel (-labelDist, labelDist);
+		function addAxisLabel (v1, v2) {
+			const labelObject = new THREE.Mesh (labelMesh, axisLabelMaterial);
+			labelObject.position[axis] = axis === -labelCenter;
+			labelObject.position[ax1] = v1;
+			labelObject.position[ax2] = v2;
+			if (axis === 'y') labelObject.rotation.z = Math.PI/2;
+			if (axis === 'z') labelObject.rotation.y = -Math.PI/2;
+			scene.add (labelObject);
+			axisLabels.push (labelObject);
+			//const positiveObject = new THREE.Mesh (positiveMesh, axisLabelMaterial);
+			//positiveObject.position[ax1] = v1;
+			//positiveObject.position[ax2] = v2;
+			//positiveObject.position[axis] = 90;
+			//scene.add (positiveObject);
+			//axisLabels.push (positiveObject);
+			//const negativeObject = new THREE.Mesh (negativeMesh, axisLabelMaterial);
+			//negativeObject.position[ax1] = v1;
+			//negativeObject.position[ax2] = v2;
+			//negativeObject.position[axis] = -90;
+			//scene.add (negativeObject);
+			//axisLabels.push (negativeObject);
+		}
+	}
+}
+function clearLabels() {
+	while (axisLabels.length > 0) {
+		scene.remove (axisLabels.pop());
+	}
+}
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	if (controls) controls.update();
+	if (camera) {
+		renderer.render( scene, camera );
+		raycaster.setFromCamera (mouse, camera);
+		const intersects = raycaster.intersectObjects (scene.children);
+		if (intersects.length > 0) {
+		}
+	}
+};
+
+const selection = [];
+const selectionObjects = [];
+const selectedLabels = [];
+
+function addSelectedMarker (name, point) {
+	let cube2 = new THREE.Mesh( bigGeometry, highlightMaterial );
+	cube2.name = name;
+	const { x, y, z } = point;
+	cube2.position.set (x, y, z);
+	scene.add( cube2 );
+	selectionObjects.push (cube2);
+}
+
+function onDocumentMouseDown (event) {
+	event.preventDefault();
+	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+	mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+	if (camera) {
+		raycaster.setFromCamera(mouse, camera);
+		const intersects = raycaster.intersectObjects(scene.children);
+		if (intersects.length > 0) {
+			const clickType = event.altKey || event.ctrlKey ? 'ctrlClick' : 'standardClick';
+			if (clickType === 'standardClick') {
+				d3Plot.clearSelections ();
+			}
+			const labels = intersects.filter(o => o != axesObject).map(o => o.object.name);
+			console.log ({ intersects, labels });
+
+			selection.push(intersects[0]);
+			selectedLabels.push(labels[0]);
+			addSelectedMarker (labels[0], intersects[0].object.position);
+			d3Plot.setSelectedLabels (selectedLabels, clickType, labels[0]);
+		}
+	}
+}
+
+document.onmousedown = onDocumentMouseDown;
+
+animate();
+
+const groupMaterials = {};
+const sceneObjects = [];
+
+function d3Plot (model, params) {
+	console.log ({ m: 'three.d3Plot', model, params });
+
+	d3Plot.clearSelections = function () {
+		while (selection.length > 0) selection.pop();
+		while (selectedLabels.length > 0) selectedLabels.pop();
+		while (selectionObjects.length > 0) scene.remove(selectionObjects.pop());
+	};
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	document.body.appendChild( renderer.domElement );
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+	// Remove any existing scene objects.
+	while (selection.length > 0) selection.pop();
+	while (selectedLabels.length > 0) selectedLabels.pop();
+	while (selectionObjects.length > 0) {
+		scene.remove (selectionObjects.pop());
+	}
+	while (sceneObjects.length > 0) {
+		scene.remove (sceneObjects.pop());
+	}
+	clearLabels();
+	// Remove any old materials.
+	for (let Class in groupMaterials) {
+		groupMaterials[Class].dispose();
+		delete groupMaterials[Class];
+	}
+
+	// Define new axis labels.
+	addLabel ('x', params.xDimension);
+	addLabel ('y', params.yDimension);
+	addLabel ('z', params.zDimension);
+
+	// Define new materials.
+	model.getGroupColors().forEach(({Class, Color}) => {
+		groupMaterials[Class] = new THREE.MeshBasicMaterial( { color: Color } );
+	});
+	const pts = model.getCoordinateValues();
+	const cls = model.getClassesInfo();
+	let maxX = 0, maxY = 0, maxZ = 0;
+
+	for (let ii = 0; ii < pts.length; ii++) {
+		if (Math.abs(pts[ii][params.xDimension]) > maxX) maxX = Math.abs(pts[ii][params.xDimension]);
+		if (Math.abs(pts[ii][params.yDimension]) > maxY) maxY = Math.abs(pts[ii][params.yDimension]);
+		if (Math.abs(pts[ii][params.zDimension]) > maxZ) maxZ = Math.abs(pts[ii][params.zDimension]);
+	}
+
+
+	const sX = 100/maxX, sY=100/maxY, sZ=100/maxZ;
+
+	d3Plot.showMyCircle = function (name, x, y, z, unknown) {
+		addSelectedMarker (name, new THREE.Vector3(x*sX, y*sY, z*sZ));
+	};
+
+	for (let ii = 0; ii < pts.length; ii++) {
+		let cube2 = new THREE.Mesh( geometry, groupMaterials[cls[ii].Class] );
+		cube2.name = pts[ii].Id;
+		cube2.position.set (pts[ii][params.xDimension]*sX, pts[ii][params.yDimension]*sY, pts[ii][params.zDimension]*sZ);
+		scene.add( cube2 );
+		sceneObjects.push (cube2);
+	}
+	camera.position.z = 200;
+
+	d3Plot.setSelectedLabels = function (selectedLabels, clickType, lastClickText) {
+		const selection = {
+			axis: model.getAxisName(),
+			pointIds: selectedLabels,
+			clickType,
+			lastClickText
+		};
+		console.log ({ m: 'three.setSelectedLabels', selection });
+		parent.postMessage({ vanodi: {nonce: model.getNonce(), op: 'selectLabels', selection}}, '*')
+	};
+
+	return {};
+}
+
+d3Plot.selectedPoints = [];
+d3Plot.hideMyPopup = function () {};
+d3Plot.killHiliteOverlay = function () {};
+d3Plot.clearLegendHilite = function () {};
+d3Plot.showMyPopup = function () {
+//(lastClicked.Id, lastClicked[ScatterPlot.xDimensionName], lastClicked[ScatterPlot.yDimensionName], lastClicked[ScatterPlot.zDimensionName], true, true)
+};
+
+d3Plot.findAndHiliteBatch = function () {
+//(lastClicked.pointClass);
+};
+
+d3Plot.hilightTable = function () {
+//(vanodi.data.pointIds);
+};
+
---
+++ scatter3d.js
@@ -0,0 +1,222 @@
+"use strict";
+
+var ScatterPlot = {};
+
+// Extract the nonce, if any, from the document's URL.
+const myNonce = (x => x && x.length > 1 ? x[1] : '')(/[?&]nonce=([^&;]+?)(&|#|;|$)/.exec(location.search));
+
+window.addEventListener('message', processMessage, false);
+
+var registerId = setInterval (registerPlugin, 1000);
+function registerPlugin () {
+	console.log ('Attempting to register...');
+	parent.postMessage ({
+		vanodi: {
+			nonce: myNonce,
+			op: 'register',
+			name: 'scatter3d',
+			axes: [
+				{ minCoordinates: 3, maxCoordinates: 3, minCovariates: 1, maxCovariates: 1 }
+			],
+			options: [
+			]
+		}
+	}, '*');
+}
+
+// Model for plot (akin to d3Plot.BasicModel in d3-plot.js)
+function NgchmModel (axisName, axis1Values, axis2Values, axis3Values, classes, ids, classColors, plotParams) {
+	console.log ({ 'NgChmModel': plotParams });
+	function model() {}
+	if ((axis1Values.length != axis2Values.length) || (axis2Values.length != classes.length) || (classes.length != ids.length)) {
+		console.error("Input arrays to NgchmModel must be of equal length")
+	}
+	model.getAxisName = function() {
+		return axisName;
+	};
+	model.getNonce = function() {
+		return myNonce;
+	};
+	model.getCoordinateValues = function() {
+		var coordinateValues = []
+		for (var i=0; i<axis1Values.length; i++) {
+			const cv = { "Id":ids[i] };
+			cv[plotParams.xDimension] = axis1Values[i];
+			cv[plotParams.yDimension] = axis2Values[i];
+			cv[plotParams.zDimension] = axis3Values[i];
+			coordinateValues.push(cv);
+		}
+		return coordinateValues;
+	};
+	model.getClassesInfo = function() {
+		var classesInfo = []
+		for (var i=0; i<axis1Values.length; i++) {
+			classesInfo.push({"Id":ids[i],"Class":classes[i]})
+		}
+		return classesInfo
+	};
+	model.getAnnotations = function() {
+		return plotParams;
+	};
+	model.getGroupColors = function() { return classColors;};
+	return model;
+}
+
+// Parameters for plot. (akin to d3Plot.DefaultParams in d3-plot.js)
+function validateParams (vanodiParams) {
+	console.log ({ 'validateParams: vanodiParams': vanodiParams });
+	const axes = vanodiParams.axes || [];		// Heatmap axes.  Not to be confused with axes in scatter plot.
+	if (axes.length !== 1) {
+		return { errorMessage: 'Parameters must include exactly one heatmap axis' };
+	}
+	const coordinates = axes[0].coordinates || [];
+	if (coordinates.length !== 3) {
+		return { errorMessage: 'Parameters must include exactly three coordinates' };
+	}
+	const covariates = axes[0].covariates || [];
+	if (covariates.length !== 1) {
+		return { errorMessage: 'Parameters must include exactly one covariate' };
+	}
+
+	const options = vanodiParams.options || {};
+
+	const params = {
+		plotTitle : vanodiParams.plotTitle || '',
+		plotAnnotation : null,
+		groupKey : 'Class',
+		oneToManyGroup : null,
+		xDimension : coordinates[0].label,
+		yDimension : coordinates[1].label,
+		zDimension : coordinates[2].label,
+		classDimension : covariates[0].label
+	};
+	return { params };
+}
+
+
+
+function processMessage (e) {
+	console.log({ three: "have message data in the new frame. origin: "+e.origin,data:e.data});
+
+	console.log ({ three: 'got message', e });
+	if (!e.data.hasOwnProperty('vanodi')) {
+		console.log ({ three: 'Unknown message type' });
+		return;
+	}
+
+	const vanodi = e.data.vanodi;
+	if (registerId) {
+		console.log ('Clearing register event');
+		clearInterval (registerId);
+		registerId = 0;
+	}
+
+	if (vanodi.op !== 'plot' & vanodi.op !== 'makeHiLite' ) {
+		console.log ({ mar4: 'Unknown Vanodi operation: ' + vanodi.op });
+		return;
+	}
+
+	/*
+		Make hilight on scatter plot based on points clicked on map.
+		
+		If click was a standard click, clear all the selected points and highlight just the point clicked
+		If click was a ctrl click, highlight all the points, ensuring that the most recently clicked highlight is drawn last.
+	*/
+	if (vanodi.op === 'makeHiLite') {
+		if (vanodi.data.axis.toLowerCase() !== ScatterPlot.axis) {
+			return;
+		}
+		var clickType = vanodi.data.clickType; // one of: 'shiftClick', 'ctrlClick', 'standardClick'. See NgChm.DET.labelClick in NGCHM viewer code.
+		if (['shiftClick', 'ctrlClick', 'standardClick'].indexOf(clickType) < 0) {
+			console.error("'clickType' must be one of 'shiftClick', 'ctrlClick', or 'standardClick'. Setting to 'standardClick'")
+			clickType = 'standardClick';
+		}
+		var lastClickText = vanodi.data.lastClickText; // string of label name last clicked
+		var lastClicked; // will store info on the last label clicked in order to show tooltip
+		d3Plot.selectedPoints = [];
+		d3Plot.hideMyPopup();
+		d3Plot.clearSelections();
+		d3Plot.killHiliteOverlay();
+		d3Plot.clearLegendHilite();
+
+		// if points passed from message are not already in the selectedPoints list, add them to it
+		var currentSelectedPointIds = d3Plot.selectedPoints.map(function(elem) { return elem.Id }) // <-- list of names (strings) of selected points
+		for (var i=0; i < vanodi.data.pointIds.length; i++) {
+			var pId = vanodi.data.pointIds[i]  // point name from passed message
+			if (currentSelectedPointIds.indexOf(pId) < 0) {  // then this point is not already in d3Plot.selectedPoints, so add it.
+				var idx = ScatterPlot.pointIds.indexOf(pId); 
+				if (idx < 0) {
+					console.log ('makeHiLite: Unknown ' + ScatterPlot.axis + ': ' + pId);
+					continue;
+				}
+				var obj = {}
+				obj.Id = pId
+				obj[ScatterPlot.xDimensionName] = ScatterPlot[ScatterPlot.xDimensionName][idx]
+				obj[ScatterPlot.yDimensionName] = ScatterPlot[ScatterPlot.yDimensionName][idx]
+				obj[ScatterPlot.zDimensionName] = ScatterPlot[ScatterPlot.zDimensionName][idx]
+				obj.pointClass = ScatterPlot.pointClasses[idx];
+				d3Plot.selectedPoints.push(obj)
+			}
+		}
+		// highlight points on scatter plot, only showing tooltip for the very last one.
+		lastClicked = null;
+		for (var i = 0; i < d3Plot.selectedPoints.length; i++) {
+			var thisId = d3Plot.selectedPoints[i];
+			if (thisId.Id != lastClickText) {
+				d3Plot.findAndHiliteBatch(thisId.pointClass);
+				d3Plot.showMyCircle(thisId.Id, thisId[ScatterPlot.xDimensionName], thisId[ScatterPlot.yDimensionName], thisId[ScatterPlot.zDimensionName], true)
+				console.log({mar4:'NOT last clicked',thisId:thisId})
+			} else {
+				lastClicked = thisId
+				console.log({mar4:'last clicked',lastClicked:lastClicked})
+			}
+		}
+		console.log ({ m: 'three.makeHiLite', selectedPoints: d3Plot.selectedPoints, lastClicked });
+		if (lastClicked) {
+			d3Plot.findAndHiliteBatch(lastClicked.pointClass);
+			d3Plot.showMyPopup(lastClicked.Id, lastClicked[ScatterPlot.xDimensionName], lastClicked[ScatterPlot.yDimensionName], lastClicked[ScatterPlot.zDimensionName], true, true)
+			d3Plot.showMyCircle(lastClicked.Id, lastClicked[ScatterPlot.xDimensionName], lastClicked[ScatterPlot.yDimensionName], lastClicked[ScatterPlot.zDimensionName], true)
+		}
+		d3Plot.hilightTable(vanodi.data.pointIds);
+		return;
+	}
+
+	// Verify plot configuration suits our needs and extract plot parameters
+	const { errorMessage, params } = validateParams(vanodi.config);
+	if (errorMessage) {
+		alert(errorMessage);
+		return;
+	}
+
+	// Extract required data.
+	const axis = vanodi.data.axes[0];
+	const axis1Values = axis.coordinates[0].map(Number);
+	const axis2Values = axis.coordinates[1].map(Number);
+	const axis3Values = axis.coordinates[2].map(Number);
+	const pointIds = axis.fullLabels;
+	const pointClasses = axis.covariates[0];
+	const classColors = axis.covariateColors[0];
+	const axisName = vanodi.config.axes[0].axisName.toLowerCase();
+	const model = NgchmModel(axisName, axis1Values,axis2Values, axis3Values, pointClasses,pointIds,classColors,params);
+	ScatterPlot.axis = axisName;
+	ScatterPlot.xDimensionName = params.xDimension;
+	ScatterPlot.yDimensionName = params.yDimension;
+	ScatterPlot.zDimensionName = params.zDimension;
+	ScatterPlot[ScatterPlot.xDimensionName] = axis1Values;
+	ScatterPlot[ScatterPlot.yDimensionName] = axis2Values;
+	ScatterPlot[ScatterPlot.zDimensionName] = axis3Values;
+	ScatterPlot.pointIds = pointIds;
+	ScatterPlot.pointClasses = pointClasses;
+
+	// Plot it.
+	//const plotNode = document.getElementById("plot-div");
+	//while (plotNode.firstChild) plotNode.removeChild(plotNode.firstChild);
+	//const legendNode = document.getElementById("legend-div");
+	//while (legendNode.firstChild) legendNode.removeChild(legendNode.firstChild);
+	const plotObject = d3Plot(model,params);
+	//const opts = plotObject.plotOptions();
+	//opts.showTooltip = true;
+	//plotObject.plotOptions(opts);
+	//$("#Reseter").off("click");	// Clear previous function, if any.
+	//$("#Reseter").click(function() { plotObject.resetScale(); });
+}
